5c3c7026a9df24cd75440410ee74f7656fa24b36
pamarthinandini50-droid <pamarthinandini50@gmail.com>
2026-07-28 10:44:07 +0000

    js

---
+++ js/script.js
@@ -0,0 +1,5 @@
+alert("welcome to NRIIT Learning Management System")
+let heading = document.getElementById("Welcome");
+heading.innerHTML = "Welcome to Future Software Engineers"
+console.log("Heading element: ", heading) 
+
